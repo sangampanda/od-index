b443cc7a2b641a54b598ccd372e4610caf07e93b
Sangam Panda <sangampandasp9@gmail.com>
2025-04-30 12:52:22 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -45,7 +45,7 @@ module.exports = {
   protectedRoutes: ['/Engineering','/Business', '/🥟 Some test files/Protected route'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
-  email: '',
+  email: 'hahano@riseup.net',
 
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
